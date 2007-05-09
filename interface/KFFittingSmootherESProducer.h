5d3924ad121d005001a1617391226f02819b4575
Giuseppe Cerati <sha1-fba32708710cbf239b252728f58f96c6304a00dd@cern.ch>
2007-05-09 13:34:14 +0000

    added documentation

--- interface/KFFittingSmootherESProducer.h
+++ interface/KFFittingSmootherESProducer.h
@@ -1,6 +1,14 @@
 #ifndef TrackingTools_TrackFitters_KFFittingSmootherESProducer_h
 #define TrackingTools_TrackFitters_KFFittingSmootherESProducer_h
 
+/** \class KFFittingSmootherESProducer
+ *  ESProducer for the KFFittingSmoother
+ *
+ *  $Date: 2007/05/09 12:56:07 $
+ *  $Revision: 1.2.2.1 $
+ *  \author cerati
+ */
+
 #include "FWCore/Framework/interface/ESProducer.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "TrackingTools/Records/interface/TrackingComponentsRecord.h"
